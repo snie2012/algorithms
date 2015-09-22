dbd0b1fe390913a791f136dee3bbf0081d26198f
snie <snie@ncsu.edu>
2015-09-22 11:27:40 -0400

    first update

---
+++ searching/BinarySearch.py
@@ -0,0 +1,9 @@
+def binarySearch(l, value):
+    low = 0
+    high = len(l) - 1
+    while low <= high:
+        mid = (low + high) / 2
+        if l[mid] > value: high = mid-1
+        if l[mid] < value: low = mid+1
+        else: return mid
+    return -1
